fda68926cd26349ebc3c18d572cf29fb82d905d8
sebgates <sebgates@gmail.com>
2021-02-16 19:55:18 +0000

    Added an extra column in the database

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ include('includes/sidebar.php');
 <th>Image</th>
 <th>Name</th>
 <th>Price</th>
+<th>Colour</th>
 <th>Delete</th>
 <th>Edit</th>
 </tr>
@@ -63,7 +64,8 @@ include('includes/sidebar.php');
 <tr>
 <td><img src="image_uploads/<?php echo $record['image']; ?>" width="100px" height="100px" /></td>
 <td><?php echo $record['name']; ?></td>
-<td class="right"><?php echo $record['price']; ?></td>
+<td><?php echo $record['price']; ?></td>
+<td><?php echo $record['colour']; ?></td>
 <td><form action="delete_record.php" method="post"
 id="delete_record_form">
 <input type="hidden" name="record_id"
